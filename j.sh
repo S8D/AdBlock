0175bf4f080bb9c3f72f78228fee98b8e59fa8d3
Darias <DariasLuc@Gmail.Com>
2018-08-11 15:42:54 +0700

    Update

--- j.sh
+++ j.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PhienBan="20180811c"
-export SetIP="0.1.2.3";fName="hosts"
+export SetIP="0.1.2.3";fName="hosts";
 Nha="https://s8d.github.io/AdBlock";uSed="${Nha}/Sed.txt";uHost="${Nha}/Lists/iOS.txt"
 #__________________________________________________________________________________________________
 export ONLINE=1
