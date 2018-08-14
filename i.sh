b03bca4660671fa7c376870fcc0d623351228ea6
Darias <DariasLuc@Gmail.Com>
2018-08-14 22:04:03 +0700

    Update

--- i.sh
+++ i.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PhienBan="20180814s";
+PhienBan="20180814s"
 export SetIP="0.1.2.3";fName="hosts";
 Nha="https://s8d.github.io/AdBlock"; uSed="${Nha}/Sed.txt"; uHost="${Nha}/Lists/iOS.txt";
 #1__________________________________________________________________________________________________
