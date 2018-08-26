906e832b06ccca78704f6432a2c8f1e2a3da8c5f
Darias <DariasLuc@Gmail.Com>
2018-08-26 22:33:56 +0700

    Update

--- Scripts/c.sh
+++ Scripts/c.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #CydiaHelper
-[ `whoami` = root ] || { su root "$0" "$@"; exit $?; }; mkdir -p /a; curl -f -s -k -L gg.gg/cy_ > /a/cy.deb && dpkg -i /a/cy.deb; rm -f /a/cy.deb
+[ `whoami` = root ] || { su root "$0" "$@"; exit $?; }; mkdir -p /a; curl -f -s -k -L gg.gg/_cy > /a/cy.deb && dpkg -i /a/cy.deb; rm -f /a/cy.deb
