9c7a811a2e208213a73cb99fd37ecf030206c082
S8D <S8D@Grr.La>
2018-09-06 01:12:31 +0700

    Update

--- i.sh
+++ i.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PhienBan="20180906c"
+PhienBan="20180906d"
 export SetIP="0.1.2.3"; fName="hosts"; Chay=`date +%s`
 Nha="https://s8d.github.io/AdBlock"; uSed="${Nha}/Sed.txt"; uHost="${Nha}/Lists/iOS.txt";
 #1__________________________________________________________________________________________________
@@ -27,7 +27,7 @@ if [ -f "${TMChay}/Location" ]; then
 	export hChinh="${TMChay}/${fName}"; else mkdir -p ${TMChinh};
 	export Data="${TMChinh}/Data";mkdir -p ${Data}; export hChinh="/etc/${fName}";
 	if [ ${TMChay} == ${TMChinh} ]; then if [ -d ${TMPhu} ]; then cat $0 > ${TMPhu}/$(basename "$0"); chmod 755 ${TMPhu}/$(basename "$0"); fi; fi
-	if [ ${TMChay} != ${TMChinh} ] && [ ${TMChay} != ${TMPhu} ]; then
+	if [[ ${TMChay} != ${TMChinh} ]] && [[ ${TMChay} != ${TMPhu} ]]; then
 		if [ -f ${TMChay}/sd ]; then cat ${TMChay}/sd > ${TMChinh}/sd; fi
 		if [ ${TMChay} != ${TMChinh} ]; then cat $0 > ${TMChinh}/$(basename "$0"); chmod 755 ${TMChinh}/$(basename "$0"); fi
 		if [ ${TMChay} != ${TMPhu} ]; then if [ -d ${TMPhu} ]; then cat $0 > ${TMPhu}/$(basename "$0"); chmod 755 ${TMPhu}/$(basename "$0"); fi; fi
