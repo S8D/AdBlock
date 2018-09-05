af374838bda6f0aebb508022445c76dda4d76104
S8D <S8D@Grr.La>
2018-09-05 23:22:07 +0700

    Update

--- i.sh
+++ i.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PhienBan="20180905g"
+PhienBan="20180905h"
 export SetIP="0.1.2.3"; fName="hosts"; Chay=`date +%s`
 Nha="https://s8d.github.io/AdBlock"; uSed="${Nha}/Sed.txt"; uHost="${Nha}/Lists/iOS.txt";
 #1__________________________________________________________________________________________________
@@ -50,9 +50,8 @@ if [ -f "${TMuc}/Location" ]; then
 		if ! [ ${TMuc} -ef ${TMChinh} ]; then cat $0 > ${TMChinh}/$(basename "$0"); chmod 755 ${TMChinh}/$(basename "$0"); fi
 		if ! [ ${TMuc} -ef ${iMuc} ]; then if [ -d ${iMuc} ]; then cat $0 > ${iMuc}/$(basename "$0"); chmod 755 ${iMuc}/$(basename "$0"); fi; fi
 		DonRac; if [ -f ${TMChinh}/$(basename "$0") ]; then printf '\n'; 
-		echo ">>> Type sh ${TMChinh}/$(basename "$0") to run AdBlock"; NhanFim; printf '\n'; sh ${TMChinh}/$(basename "$0"); fi; exit 0;
+		echo ">>> Type sh ${TMChinh}/$(basename "$0") to run AdBlock"; NhanFim; printf '\n'; sh ${TMChinh}/$(basename "$0"); exit 0; fi
 	fi
-	#if ! [ ${TMuc} -ef ${TMChinh} ] then sh ${TMChinh}/$(basename "$0"); fi; exit 0;
 fi
 export fSed="${MTam}/Sed";export fHost="${Data}/Host.txt";
 export tam="${MTam}/t.tmp";export hTam="${MTam}/h.tmp";
