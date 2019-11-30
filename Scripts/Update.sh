b5a06ad69828d84da3ebf38ad2e20f7a6f011e05
S8D <DariasLuc@Gmail.Com>
2019-11-30 09:00:15 +0700

    Update Update.sh

--- Scripts/Update.sh
+++ Scripts/Update.sh
@@ -1,31 +1,33 @@
-mkdir -p /usr/local/sbin && cat > /usr/local/sbin/Update.sh << \EOF
+cat > /root/u.sh << \EOF
 
 #!/bin/ash
+Log="/www/Update.log"
+if [ ! -f "$Log" ]; then echo > $Log; fi;
 opkg update && opkg upgrade netifd
 opkg list-installed | grep -qw luci-ssl || opkg install luci-ssl && /etc/init.d/uhttpd restart
-PACKAGES="$(opkg list-upgradable |awk '{print $1}')"
-if [ -n "${PACKAGES}" ]; then
-  opkg upgrade ${PACKAGES}
+Goi="$(opkg list-upgradable |awk '{print $1}')"
+if [ -n "${Goi}" ]; then
+  opkg upgrade ${Goi}
   if [ "$?" -eq 0 ]; then
-    echo "$(date -I"seconds") - update success, rebooting" \
->> /www/Update.log
+    echo "$(date +"%F %a %T") - Cập nhật thành công" \
+>> $Log
     exec reboot
   else
-    echo "$(date -I"seconds") - update failed" >> /www/Update.log
+    echo "$(date +"%F %a %T") - Cập nhật thất bại" >> $Log
   fi
 else
-  echo "$(date -I"seconds") - nothing to update" >> /www/Update.log
+  echo "$(date +"%F %a %T") - Không có cập nhật" >> $Log
 fi
-cat /www/Update.log
+cat $Log
 EOF
 
-chmod u+x /usr/local/sbin/Update.sh && touch /www/Update.log && echo "/usr/local/sbin/" >> /etc/sysupgrade.conf && echo "/www/Update.log" >> /etc/sysupgrade.conf
+chmod u+x /root/u.sh && touch /www/Update.log && echo "/root/" >> /etc/sysupgrade.conf && echo "/www/Update.log" >> /etc/sysupgrade.conf
 
 crontab -e
-0 2 * * * /usr/local/sbin/Update.sh
+0 2 * * * sh /root/u.sh
 
 /etc/init.d/cron start && /etc/init.d/cron enable
 
-/usr/local/sbin/Update.sh
+sh /root/u.sh
 
 curl http://router/Update.log
