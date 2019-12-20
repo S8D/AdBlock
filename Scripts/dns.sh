31eb56401792f1bee429c0b3a371be2beb23d35d
Darias <DariasLuc@Gmail.Com>
2019-12-21 02:36:27 +0700

    Update dns.sh

--- Scripts/dns.sh
+++ Scripts/dns.sh
@@ -1,45 +1,80 @@
-cat > /root/dns.sh << \EOF
-
-#!/bin/ash
 #!/bin/bash
-Log="/www/Update.log"
+PhienBan=20191221a
+TM="/data/media/0/dnscrypt-proxy"
+FileTam="${TM}/tam"
+Update="gg.gg/dns_"
+Log="${TM}/Update.log"
+File="${TM}/DNSCrypt.zip"
+[ `whoami` = root ] || { echo "Đã cấp quyền SU. Chạy lại $0"; su "$0" "$@"; exit $?; };
 if [ ! -f "$Log" ]; then echo > $Log; fi;
-opkg list-installed | grep -qw luci-ssl || {
-	echo "$(date +"%F %a %T") - Đang cài SSL"; opkg update; opkg install luci-ssl;
-}
+cd $TM
+OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"
+if [ $OS == $x64 ]; then TenFile="linux_x86_64"; ThuMucNen="linux-x86_64"; fi
+if [ $OS == $arm ]; then TenFile="linux_arm-"; ThuMucNen="linux-arm"; fi
+if [ $OS == $Android ]; then TenFile="android_arm64"; ThuMucNen="android-arm64"; fi
+#___1____________________________________________________________________________________________
+CheckNet() {
+	Servers="\
+	https://g.co \
+	https://t.co \
+	https://m.co \
+	"
+	processes="0"
+	pids=""
 
-opkg list-installed | grep -qw curl || {
-	echo "$(date +"%F %a %T") - Đang cài cURL"; opkg update; opkg install curl;
-}
+	for Server in $Servers; do
+		curl --silent --head "$Server" > /dev/null &
+		pids="$pids $!"
+		processes=$(($processes + 1))
+	done
 
-opkg list-installed | grep -qw wget || {
-	echo "$(date +"%F %a %T") - Đang cài wGet"; opkg update; opkg install wget;
+	while [ $processes -gt 0 ]; do
+		for pid in $pids; do
+			if ! ps | grep "^[[:blank:]]*$pid[[:blank:]]" > /dev/null; then
+				processes=$(($processes - 1))
+				pids=$(echo "$pids" | sed --regexp-extended "s/(^| )$pid($| )/ /g")
+				if wait $pid; then
+					kill -TERM $pids > /dev/null 2>&1 || true
+					wait $pids
+					return 0
+				fi
+			fi
+		done
+		sleep 0.1
+	done
+	return 1
 }
-PhienBanOn=$(curl -s "https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest" | awk -F '"' '/tag_name/{print $4}')
-PhienBanOff=$(dnscrypt-proxy --version)
-if [ $PhienBanOn == $PhienBanOff ]; then echo "DNSCrypt-Proxy $PhienBanOn là phiên bản mới nhất" >> $Log;
-echo "DNSCrypt-Proxy $PhienBanOn là phiên bản mới nhất"; exit; else
-OS=`uname --m`; x64="x86_64"; arm="armv7l";
-if [ $OS == $x64 ]; then linktai="linux_x86_64"; giainen="linux-x86_64"; fi
-if [ $OS == $arm ]; then linktai="linux_arm-"; giainen="linux-arm"; fi
-echo "OS: $OS | URL: $linktai | Local: $giainen"
-echo "$(date +"%F %a %T") - Đang tải DNSCrypt-Proxy"
-curl -s https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest \
-| grep browser_download_url.*tar.gz \
-| grep $linktai \
-| cut -d '"' -f 4 \
-| wget -qi -
-echo "$(date +"%F %a %T") - Đang giải nén DNSCrypt-Proxy"
-tar xzf /root/*.tar.gz; chmod +x /root/$giainen/dnscrypt-proxy
-/etc/init.d/dnscrypt-proxy stop
-echo "$(date +"%F %a %T") - Đang cập nhật DNSCrypt-Proxy"
-mv /root/$giainen/dnscrypt-proxy /usr/sbin/dnscrypt-proxy
-/etc/init.d/dnscrypt-proxy start
-rm -r /root/*.gz
-rm -r /root/*.minisig
-rm -r /root/$giainen/
-echo "$(date +"%F %a %T") - DNSCrypt-Proxy đã được cập nhật lên phiên bản $PhienBanOn" >> $Log
-echo "$(date +"%F %a %T") - DNSCrypt-Proxy đã được cập nhật lên phiên bản $PhienBanOn"
+#___2____________________________________________________________________________________________
+if CheckNet; then echo "$(date +"%F %a %T") - Đang kiểm tra phiên bản $(basename "$0")"; curl -s -L -o $FileTam $Update
+	PhienBanMoi=$(cat ${FileTam} | grep .*PhienBan | sed 's/.*\=//'); echo "$PhienBanMoi"
+	if [ $PhienBanMoi == $PhienBan ]; then
+		echo "$(date +"%F %a %T") - Đang kiểm tra phiên bản DNSCrypt-Proxy"
+		PhienBanOn=$(curl -s "https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest" | awk -F '"' '/tag_name/{print $4}')
+		PhienBanOff=$(dnscrypt-proxy --version)
+		echo "OS: $OS | FileName: dnscrypt-proxy-$TenFile-$PhienBanOn | Local: $ThuMucNen"
+		if [ $PhienBanOn == $PhienBanOff ]; then
+			echo "$(date +"%F %a %T") - DNSCrypt-Proxy $PhienBanOn là phiên bản mới nhất" >> $Log;
+			echo "DNSCrypt-Proxy $PhienBanOn là phiên bản mới nhất"; exit; else
+			echo "Đang cập nhật DNSCrypt-Proxy $PhienBanOff lên phiên bản $PhienBanOn"
+			echo "$(date +"%F %a %T") - Đang tải DNSCrypt-Proxy"
+			LinkTai=$(curl -s https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest \
+			| grep browser_download_url.*zip \
+			| grep $TenFile \
+			| cut -d '"' -f 4)
+			curl -s -L -o $File $LinkTai; fi
+	else chmod +x $FileTam; cp $0 ${TM}/$0\_$PhienBan.sh; mv $FileTam $(basename "$0")
+		echo ">>> Starting $(basename "$0") $PhienBanMoi..."; $0 $@; exit; fi
+else echo "$(date +"%F %a %T") - Không có Internet!!! Đang thoát..."; exit; fi
+#___3____________________________________________________________________________________________
+if [[ -f "$File" ]]; then
+	echo "$(date +"%F %a %T") - Đang giải nén DNSCrypt-Proxy"
+	rm -r ${TM}/${ThuMucNen}/
+	unzip ${TM}/DNSCrypt.zip; chmod +x ${TM}/${ThuMucNen}/dnscrypt-proxy
+	pkill -HUP dnscrypt-proxy
+	echo "$(date +"%F %a %T") - Đang cập nhật DNSCrypt-Proxy"
+	mv $TM/${ThuMucNen}/dnscrypt-proxy /system/bin/dnscrypt-proxy
+	rm -r ${TM}/*.zip; rm -r ${TM}/*.minisig; rm -r ${TM}/${ThuMucNen}/; rm -r $FileTam
+	echo "$(date +"%F %a %T") - DNSCrypt-Proxy được cập nhật lên $PhienBanOn" >> $Log
+	echo "$(date +"%F %a %T") - DNSCrypt-Proxy đã được cập nhật lên phiên bản $PhienBanOn"; else
+	echo "$(date +"%F %a %T") - Không tìm thấy $File!!! Đang thoát..."; exit
 fi
-
-EOF
