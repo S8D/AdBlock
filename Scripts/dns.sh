5410d2a27d7c13433e97a7ce1aec6339adc63015
Darias <DariasLuc@Gmail.Com>
2019-12-21 13:19:31 +0700

    Update dns.sh

--- Scripts/dns.sh
+++ Scripts/dns.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-PhienBan="20191221g"
-Time=$(date +"%F %a %T");
-OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64";
-if [ $OS == $x64 ]; then TenFile="linux_x86_64"; ThuMucNen="linux-x86_64"; TMGoc="/root/dns"; GiaiNen="tar -xzf"; File="${TMGoc}/DNSCrypt.tar.gz"; fi;
-if [ $OS == $arm ]; then TenFile="linux_arm-"; ThuMucNen="linux-arm"; TMGoc="/root/dns"; GiaiNen="tar -xzf"; File="${TMGoc}/DNSCrypt.tar.gz"; fi;
+PhienBan="20191221h"
+Time=$(date +"%F %a %T")
+OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"
+if [ $OS == $x64 ]; then TenFile="linux_x86_64"; ThuMucNen="linux-x86_64"; TMGoc="/root/dns"; GiaiNen="tar -xzf"; File="${TMGoc}/DNSCrypt.tar.gz"; fi
+if [ $OS == $arm ]; then TenFile="linux_arm-"; ThuMucNen="linux-arm"; TMGoc="/root/dns"; GiaiNen="tar -xzf"; File="${TMGoc}/DNSCrypt.tar.gz"; fi
 if [ $OS == $Android ]; then TenFile="android_arm64"; ThuMucNen="android-arm64"; 
 	TMGoc="/sdcard/dnscrypt-proxy"; GiaiNen="unzip"; File="${TMGoc}/DNSCrypt.zip";
-	[ `whoami` = root ] || { echo "$Time - Đã cấp quyền SU. Chạy lại $0"; su "$0" "$@"; exit $?; }; fi;
-mkdir -p ${TMGoc};
-FileTam="${TMGoc}/tam";
-Update="gg.gg/_dns";
-Log="${TMGoc}/Update.log";
-if [ ! -f "$Log" ]; then echo > $Log; fi;
+	[ `whoami` = root ] || { echo "$Time - Đã cấp quyền SU. Chạy lại $0"; su "$0" "$@"; exit $?; }; fi
+mkdir -p ${TMGoc}
+FileTam="${TMGoc}/tam"
+Update="gg.gg/_dns"
+Log="${TMGoc}/Update.log"
+if [ ! -f "$Log" ]; then echo > $Log; fi
 #___1____________________________________________________________________________________________
 IP=$(curl -s 'http://checkip.dyndns.org' | sed 's/.*Current IP Address: \([0-9\.]*\).*/\1/g')
 if [ "$IP" != "" ] && [ "$IP" != "0.0.0.0" ]; then
@@ -31,13 +31,13 @@ if [ "$IP" != "" ] && [ "$IP" != "0.0.0.0" ]; then
 			echo "$Time - Đang tải DNSCrypt-Proxy";
 			LinkTai=$(curl -s gg.gg/dns_ | grep browser_download_url.*zip | grep $TenFile | cut -d '"' -f 4);
 			curl -s -L -o $File $LinkTai;
-		fi;
+		fi
 	else
 		chmod +x $FileTam; cp $0 ${TMGoc}/$0\_$PhienBan.sh; mv $FileTam $(basename "$0");
 		echo "$Time - Starting $(basename "$0") $PhienBanMoi..."; sh $0; exit;
-	fi;
+	fi
 else echo "$Time - Không có Internet!!! Đang thoát..."; exit;
-fi;
+fi
 #___3____________________________________________________________________________________________
 if [[ -f "$File" ]]; then
 	echo "$Time - Đang giải nén DNSCrypt-Proxy";
@@ -50,4 +50,4 @@ if [[ -f "$File" ]]; then
 	echo "$Time - DNSCrypt-Proxy được cập nhật lên $PhienBanOn" >> $Log;
 	echo "$Time - DNSCrypt-Proxy đã được cập nhật lên phiên bản $PhienBanOn"; else
 	echo "$Time - Không tìm thấy $File!!! Đang thoát..."; exit;
-fi;
+fi
