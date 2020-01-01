2fcc26c631ed7f091d12f6818efff197587745f4
Darias <DariasLuc@Gmail.Com>
2020-01-01 23:43:41 +0700

    ...

--- Scripts/dns.sh
+++ Scripts/dns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PhienBan="20200101i"
+PhienBan="20200101k"
 Time=$(date +"%F %a %T");
 echo "$(basename "$0") phiên bản $PhienBan"
 OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"
@@ -17,9 +17,8 @@ if [ $net -eq 1 ]; then
 if [ $PhienBanMoi == $PhienBan ]; then echo "$Time - $(basename "$0") $PhienBan là phiên bản mới nhất";
 else echo "$Time - Đang cập nhật $(basename "$0") $PhienBan lên phiên bản $PhienBanMoi";
 cp $(basename "$0") ${TM}/$PhienBan\_$(basename "$0")
-curl -s -L -o $upTam gg.gg/_dns; mv 
-mv $upTam ${TM}/$(basename "$0")
-echo "$Time - Khởi chạy $(basename "$0") $PhienBanMoi..."; $0 $@; exit 1; fi; fi
+curl -s -L -o $upTam gg.gg/_dns; mv $upTam ${TM}/$(basename "$0")
+echo "$Time - Khởi chạy $(basename "$0") $PhienBanMoi..."; sh ${TM}/$(basename "$0"); exit 1; fi; fi
 #___________________________________________________________________________________________________________________________________________________
 if [ $net -eq 1 ]; then echo "$Time - Đang kiểm tra phiên bản DNSCrypt-Proxy" ; else echo "$(date +"%F %a %T") - Kiểm tra lại Internet"; exit; fi
 PhienBanOn=$(curl -s -L "gg.gg/dns_" | awk -F '"' '/tag_name/{print $4}')
