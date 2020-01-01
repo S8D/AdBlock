5bed4af082dc1f7a7a51213d3b1c7a12fe196b77
Darias <DariasLuc@Gmail.Com>
2020-01-02 03:38:16 +0700

    ..

--- Scripts/dns.sh
+++ Scripts/dns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PhienBan="20200102g"
+PhienBan="20200102h"
 Time=$(date +"%F %a %T");
 echo "$(basename "$0") phiên bản $PhienBan"
 OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"
@@ -17,7 +17,7 @@ if [ $PhienBanMoi == $PhienBan ]; then echo "$Time - $(basename "$0") $PhienBan 
 else echo "$Time - Đang cập nhật $(basename "$0") $PhienBan lên phiên bản $PhienBanMoi";
 cp $0 ${TM}/dns/$PhienBan\_$(basename "$0")
 curl -s -L -o $upTam gg.gg/_dns; chmod+x $upTam; mv $upTam ${TM}/$0
-echo "$Time - Khởi chạy $(basename "$0") $PhienBanMoi..."; sh ${TM}/$0; exit 1; fi; fi
+echo "$Time - Khởi chạy $(basename "$0") $PhienBanMoi..."; sh ${TM}/$(basename "$0"); exit 1; fi; fi
 #___________________________________________________________________________________________________________________________________________________
 if [ $net -eq 1 ]; then echo "$Time - Đang kiểm tra phiên bản DNSCrypt-Proxy" ; else echo "$(date +"%F %a %T") - Kiểm tra lại Internet"; exit; fi
 PhienBanOn=$(curl -s -L "gg.gg/dns_" | awk -F '"' '/tag_name/{print $4}')
