6077b2bd54523b9f678d1627176e31f8739ae026
Darias <DariasLuc@Gmail.Com>
2020-01-04 14:37:18 +0700

    Update dns.sh

--- Scripts/dns.sh
+++ Scripts/dns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PhienBan="20200104a"
+PhienBan="20200104b"
 #GetTime=$(date +"%F %a %T"); Time="$GetTime -"
 Time="#"
 #echo "$Time $(basename "$0") phiên bản $PhienBan"
@@ -19,31 +19,36 @@ if [ $OS == $Android ]; then
 Log="${TMLog}/Update.log"; if [ ! -f "$Log" ]; then echo > $Log; fi;
 upTam="${TM}/dns/tam"; rm -f $upTam
 
-echo "$Time Đang kiểm tra cập nhật $(basename "$0") $PhienBan..."
-CheckNet () { ping -q -c 1 -W 1 g.co >/dev/null; };
-if CheckNet; then net=1; else net=0; fi
-if [ $net -eq 1 ]; then PhienBanMoi=$(curl -s -L "gg.gg/_dns" | grep PhienBan\= | sed 's/.*\=\"//; s/\"$//');
+echo "$Time Đang kiểm tra máy chủ cập nhật..."
+CheckTN () { ping -q -c 1 -W 1 tiny.cc >/dev/null; }; CheckGG () { ping -q -c 1 -W 1 gg.gg >/dev/null; }; CheckGL () { ping -q -c 1 -W 1 g.co >/dev/null; }; 
+if CheckGG; then UpLink="gg.gg/_dns"; DownLink="gg.gg/dns_"; net="1"; else
+	if CheckTN; then UpLink="https://tiny.cc/_dns"; DownLink="https://tiny.cc/dns_"; net="2"; else
+		if CheckGL; then UpLink="https://s8d.github.io/AdBlock/Scripts/dns.sh"; DownLink="https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest"; net="3"; else net=0; fi; fi; fi
+if [ $net -ge 1 ]; then echo "$Time Đang kiểm tra cập nhật $(basename "$0") $PhienBan..."
+	PhienBanMoi=$(curl -s -L "${UpLink}" | grep PhienBan\= | sed 's/.*\=\"//; s/\"$//');
 	if [ $PhienBanMoi == $PhienBan ]; then echo "$Time $(basename "$0") $PhienBan là bản mới nhất!";
 	else echo "$Time Đang cập nhật $(basename "$0") v.$PhienBan lên v.$PhienBanMoi...";
 		cp $0 ${TM}/dns/$PhienBan\_$(basename "$0")
-		curl -s -L -o $upTam gg.gg/_dns; chmod +x $upTam; mv $upTam ${TM}/$0
+		curl -s -L -o $upTam $UpLink; chmod +x $upTam; mv $upTam ${TM}/$0
 		echo "$Time Khởi chạy $(basename "$0") $PhienBanMoi..."; sh ${TM}/$(basename "$0"); exit 1; fi; fi
 if [ $net -eq 1 ]; then echo "$Time Đang kiểm tra cập nhật DNSCrypt-Proxy..." ;
-else echo "$Time Kiểm tra lại Internet"; exit; fi
+else echo "$Time Không có kết nối Internet"; exit; fi
 
-PhienBanOn=$(curl -s -L "gg.gg/dns_" | awk -F '"' '/tag_name/{print $4}')
+PhienBanOn=$(curl -s -L "${DownLink}" | awk -F '"' '/tag_name/{print $4}')
 PhienBanOff=$(dns --version)
 
 if [ $PhienBanOn == $PhienBanOff ]; then echo "$Time DNSCrypt-Proxy $PhienBanOn là phiên bản mới nhất!" >> $Log;
 	echo "$Time DNSCrypt-Proxy $PhienBanOn là bản mới nhất!"; exit 1; else
 	echo "$Time Đang cập nhật DNSCrypt-Proxy v.$PhienBanOff lên v.$PhienBanOn..."
 	echo "$Time Đang tải DNSCrypt-Proxy..."
-	DownURL=$(curl -s -L gg.gg/dns_ | grep browser_download_url.*$duoi | grep $linktai | cut -d '"' -f 4)
+	DownURL=$(curl -s -L $DownLink | grep browser_download_url.*$duoi | grep $linktai | cut -d '"' -f 4)
 	curl -s -L -o $TM/DNSCrypt.$duoi $DownURL
+	
 	echo "$Time Đang giải nén DNSCrypt-Proxy..."; rm -rf ${TM}/${ThuMuc}
 	$giainen ${TM}/DNSCrypt.$duoi; chmod +x ${TM}/${ThuMuc}/dnscrypt-proxy
 	if [ $OS == $x64 ] || [ $OS == $arm ]; then /etc/init.d/dns stop; dns="/usr/sbin/dns"; fi
-	if [ $OS == $Android ]; then pkill -HUP dns; dns="/system/bin/dns"; fi
+	if [ $OS == $Android ]; then pkill -HUP dns; pkill -HUP dnscrypt-proxy; dns="/system/bin/dns"; fi
+	
 	echo "$Time Đang cập nhật DNSCrypt-Proxy..."
 	mv ${TM}/${ThuMuc}/dnscrypt-proxy $dns
 	if [ $OS == $x64 ] || [ $OS == $arm ]; then /etc/init.d/dns start; fi
