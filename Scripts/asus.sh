b9e99afe4ec6f43b5f8c5415ce407638cb004ad4
Darias <DariasLuc@Gmail.Com>
2020-01-12 16:46:30 +0700

    Update asus.sh

--- Scripts/asus.sh
+++ Scripts/asus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PhienBan="20200112n"
-DNSCRYPT_VER=2.0.36-beta.1
-#DNSCRYPT_VER=2.0.36
+#DNSCRYPT_VER=2.0.36-beta.1
+DNSCRYPT_VER=2.0.36
 
 GetTime=$(date +"%F %a %T"); Time="$GetTime -"; DauCau="#"
 TM="/opt"; tmDNS="${TM}/dns"; mkdir -p $tmDNS; 
