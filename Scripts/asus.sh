a7bd20139f52beb4b10a6ad172f84acb90cb7527
Darias <DariasLuc@Gmail.Com>
2020-01-12 14:02:58 +0700

    Update asus.sh

--- Scripts/asus.sh
+++ Scripts/asus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PhienBan="20200112e"
+PhienBan="20200112f"
 DNSCRYPT_VER=2.0.36-beta.1
 #DNSCRYPT_VER=2.0.36
 
@@ -45,44 +45,15 @@ if [ $PhienBanOn == $PhienBanOff ]; then echo "$Time DNSCrypt-Proxy $PhienBanOn 
   $dl1 $TM/DNSCrypt.tar.gz $DownURL
   
   echo "$DauCau Đang giải nén DNSCrypt-Proxy..."; rm -rf ${TM}/${ThuMuc}
-  tar xzv -C $TM -f $TM/DNSCrypt.tar.gz ; chmod +x ${TM}/${ThuMuc}/dnscrypt-proxy
-  
+  tar xzv -C $TM -f $TM/DNSCrypt.tar.gz ; chmod +x ${TM}/${ThuMuc}/dnscrypt-proxy  
   echo "$DauCau Đang cập nhật DNSCrypt-Proxy..."
-  if [ $? -ne 0 ]; then
-    echo -e "$ERROR Unable to download dnscrypt-proxy package for your router"
-    end_op_message
-    return
-  fi
-  chown `nvram get http_username`:root ${TM}/${ThuMuc}/*
-  
-  mv ${TM}/${ThuMuc}/dnscrypt-proxy $dns; chmod +x $dns
-
-  del_jffs_script /jffs/scripts/wan-start dnscrypt-start
-  write_manager_script /jffs/scripts/dnsmasq.postconf dnsmasq
-  write_manager_script /jffs/scripts/init-start init-start
-  write_manager_script /jffs/scripts/wan-start wan-start
-  setup_dnscrypt
-  if [ $? -ne 0 ]; then
-    end_op_message
-    return
-  fi
-
-  echo -e "$INFO Staring dnscrypt-proxy..."
-  $TARG_DIR/manager dnscrypt-start
-  sleep 1
-  if [ -z "`pidof dnscrypt-proxy`" ]; then
-    echo -e "$ERROR Couldn't start dnscrypt-proxy"
-    echo -e "$ERROR Please send WebUI System Log to dev"
-    end_op_message
-    return
-  fi
-  service restart_dnsmasq
-  manager_monitor_restart
+  chown `nvram get http_username`:root ${TM}/${ThuMuc}/dnscrypt-proxy
+  mv ${TM}/${ThuMuc}/dnscrypt-proxy $dns
 
   rm -rf ${TM}/${ThuMuc}; rm -f ${TM}/DNSCrypt.$duoi; rm -f $upTam;
   PhienBanOn=$(${dl2} "${DownLink}" | awk -F '"' '/tag_name/{print $4}'); PhienBanOff=$(${dns} --version)
   if [ $PhienBanOn == $PhienBanOff ]; then echo "$Time DNSCrypt-Proxy đã được cập nhật lên $PhienBanOn" >> $Log;
-    echo "$DauCau DNSCrypt-Proxy đã được cập nhật lên v.$PhienBanOn"; else
+    echo "$DauCau DNSCrypt-Proxy đã được cập nhật lên v.$PhienBanOn"; $dns --config $CauHinh; else
     echo "$DauCau Cập nhật DNSCrypt-Proxy v.$PhienBanOff lên v.$PhienBanOn thất bại!!!"; fi
 fi
 
