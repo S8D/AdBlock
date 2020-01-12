67ccc7291d989ca12d157500579e417859b39b4c
Darias <DariasLuc@Gmail.Com>
2020-01-12 11:36:12 +0700

    Update asus.sh

--- Scripts/asus.sh
+++ Scripts/asus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PhienBan="20200112j"
+PhienBan="20200112k"
 #GetTime=$(date +"%F %a %T"); Time="$GetTime -"
 Time="#"
 #echo "$Time $(basename "$0") phiên bản $PhienBan"
@@ -7,13 +7,10 @@ dns="/jffs/dnscrypt/dnscrypt-proxy"
 asus="/jffs/asus"
 dl1="curl -s -L -o"; dl2="curl -s -L"
 OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"
-if [ $OS == $x64 ]; then linktai="linux_x86_64"; ThuMuc="linux-x86_64"; duoi="tar.gz"; giainen="tar -C ${TM} -xvf"; TM="/root"; TMLog="/www"; fi
 if [ $OS == $arm ]; then linktai="linux_arm-"; ThuMuc="linux-arm"; duoi="tar.gz"; 
-  giainen="tar -C ${TM} -xvf"; TM="/jffs"; TMLog="${TM}/dns"; mkdir -p $TMLog; fi
-if [ $OS == $Android ]; then linktai="android_arm64"; ThuMuc="android-arm64"; duoi="zip"; giainen="unzip -d "${TM}""; TM="/sdcard"; TMLog="${TM}/dns"; 
-[ "$(whoami)" != "root" ] && { echo "Đã lấy SU, hãy chạy lại $(basename "$0")"; exec su "$0" "$@"; }; fi
-#echo "OS: $OS | URL: $linktai | Local: $TM | Folder: $ThuMuc | Extract: $giainen | Filetype: $duoi"
-Log="${TMLog}/Update.log"; if [ ! -f "$Log" ]; then echo '' > $Log; fi;
+  giainen="tar -C ${TM} -xvf"; TM="/jffs"; tmDNS="${TM}/dns"; mkdir -p $tmDNS; else
+  echo "$Time Scripts chưa hỗ trợ nền tảng đang chạy"; exit 1; fi
+Log="${tmDNS}/Update.log"; if [ ! -f "$Log" ]; then echo '' > $Log; fi;
 upTam="${TM}/tam"; rm -f $upTam;
 echo "$Time Đang kiểm tra cập nhật $(basename "$0") $PhienBan..."
 CheckNet () { ping -q -c 1 -W 1 g.co >/dev/null; };
@@ -21,7 +18,7 @@ if CheckNet; then net=1; else net=0; fi
 if [ $net -eq 1 ]; then PhienBanMoi=$(curl -s -L "gg.gg/_asus" | grep PhienBan\= | sed 's/.*\=\"//; s/\"$//');
 if [ $PhienBanMoi == $PhienBan ]; then echo "$Time $(basename "$0") $PhienBan là bản mới nhất!";
 else echo "$Time Đang cập nhật $(basename "$0") v.$PhienBan lên v.$PhienBanMoi...";
-cp $0 ${TM}/dns/$PhienBan\_$(basename "$0")
+cp $0 ${tmDNS}/$PhienBan\_$(basename "$0")
 curl -s -L -o $upTam gg.gg/_asus; chmod +x $upTam; mv $upTam ${TM}/$0
 echo "$Time Khởi chạy $(basename "$0") $PhienBanMoi..."; sh ${TM}/$(basename "$0"); exit 1; fi; fi
 
