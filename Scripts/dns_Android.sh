5ac74217b22d5882fbd10614e343c505dd12e651
Darias <DariasLuc@Gmail.Com>
2020-01-18 22:24:14 +0700

    ...

--- Scripts/dns_Android.sh
+++ Scripts/dns_Android.sh
@@ -1,2 +1,2 @@
-TatDNS==pgrep dns;pkill dns;pgrep dns >/dev/null
+TatDNS==pgrep dns; pkill dns; pgrep dns; pkill dns; pgrep dns; pkill dns; pgrep dns >/dev/null
 GoiDNS==echo "dns --config /sdcard/dns/CauHinh.toml"
