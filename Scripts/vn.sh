4edc47bae0984c63e7824f14f729c9219000f1de
Darias <DariasLuc@Gmail.Com>
2020-01-30 09:58:44 +0700

    ...

--- Scripts/vn.sh
+++ Scripts/vn.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-PhienBan="20200130b"
+PhienBan="20200130c"
 OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"
-if [ $OS == $x64 ]; then if [ -d /www/cgi-bin ]; then sp="/usr/sbin/sp"; echo "OS: $OS | OpenWRT"; else sp="/sbin/sp"; echo "OS: $OS"; fi
-if [ ! -f "$sp" ]; then curl -sLo $sp uli.vn/_sp ;fi; fi
-if [ $OS == $arm ]; then if [ -d /www/cgi-bin ]; then sp="/usr/sbin/sp"; echo "OS: $OS | OpenWRT"; else sp="/opt/sbin/sp"; echo "OS: $OS"; fi
-if [ ! -f "$sp" ]; then curl -sLo $sp uli.vn/sp_ ;fi; fi
-if [ $OS == $Android ]; then sp="/system/xbin/sp"; echo "OS: $OS"; if [ ! -f "$sp" ]; then curl -sLo $sp uli.vn/sp- ;fi; fi
+if [ $OS == $x64 ]; then if [ -d /www/cgi-bin ]; then sp="/usr/sbin/sp"; echo "OS: $OS | OpenWRT"; 
+else sp="/sbin/sp"; echo "OS: $OS"; fi; if [ ! -f "$sp" ]; then 
+	echo "Đang tải SpeedTest"; curl -sLo $sp uli.vn/_sp ;fi; fi
+
+if [ $OS == $arm ]; then if [ -d /www/cgi-bin ]; then sp="/usr/sbin/sp"; echo "OS: $OS | OpenWRT"; 
+else sp="/opt/sbin/sp"; echo "OS: $OS"; fi; if [ ! -f "$sp" ]; then 
+	echo "Đang tải SpeedTest"; curl -sLo $sp uli.vn/sp_ ;fi; fi
+
+if [ $OS == $Android ]; then sp="/system/xbin/sp"; echo "OS: $OS"; 
+	if [ ! -f "$sp" ]; then echo "Đang tải SpeedTest"; curl -sLo $sp uli.vn/sp- ;fi; fi
+
 echo "Kiểm tra tốc độ mạng tới máy chủ Việt Nam"
 sp -B -s 6106
