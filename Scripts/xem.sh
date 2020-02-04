e33b1929083af566c47d197826b129c0454d0fe7
Darias <DariasLuc@Gmail.Com>
2020-02-04 11:38:44 +0700

    Create xem.sh

---
+++ Scripts/xem.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"
+if [ $OS == $x64 ] || [ $OS == $arm ]; then TM="/root"; fi; if [ $OS == $Android ]; then TM="/sdcard"; fi
+tmDNS="${TM}/dns"; tmDNS="/root/dns";
+Den="${tmDNS}/Den.txt";
+ipDen="${tmDNS}/ipDen.txt";
+Trang="${tmDNS}/Trang.txt";
+Choang="${tmDNS}/Choang.txt";
+Chuyen="${tmDNS}/Chuyen.txt";
+echo "$DauCau Phiên bản Bộ lọc:";
+vDen=$(cat $Den | grep .*PhienBan\_ | sed 's/.*\_//');
+vipDen=$(cat $ipDen | grep .*PhienBan\_ | sed 's/.*\_//');
+vTrang=$(cat $Trang | grep .*PhienBan\_ | sed 's/.*\_//');
+vChoang=$(cat $Choang | grep .*PhienBan\_ | sed 's/.*\_//');
+vChuyen=$(cat $Chuyen | grep .*PhienBan\_ | sed 's/.*\_//');
+echo "$DauCau Đen: $vDen | Choàng: $vChoang | Chuyển: $vChuyen | ipDen: $vipDen | Trắng: $vTrang"
