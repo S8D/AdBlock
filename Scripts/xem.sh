229604dde00d1d4b02fd99c2d486a3c75acb27d2
Darias <DariasLuc@Gmail.Com>
2020-02-04 12:02:56 +0700

    Update xem.sh

--- Scripts/xem.sh
+++ Scripts/xem.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PhienBan="20190204b"
+PhienBan="20190204c"
 DauCau="#"
 OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64";
 if [ $OS == $x64 ] || [ $OS == $arm ]; then TM="/root"; fi; 
@@ -15,4 +15,4 @@ vipDen=$(cat $ipDen | grep .*PhienBan\_ | sed 's/.*\_//');
 vTrang=$(cat $Trang | grep .*PhienBan\_ | sed 's/.*\_//');
 vChoang=$(cat $Choang | grep .*PhienBan\_ | sed 's/.*\_//');
 vChuyen=$(cat $Chuyen | grep .*PhienBan\_ | sed 's/.*\_//');
-echo -e "$DauCau Phiên bản Bộ lọc:\n Đen: $vDen\n Choàng: $vChoang\n Chuyển: $vChuyen\n ipDen: $vipDen\n Trắng: $vTrang"
+echo -e "$DauCau Phiên bản Bộ lọc:\n Đen:\t$vDen\n Choàng:\t$vChoang\n Chuyển:\t$vChuyen\n ipDen:\t$vipDen\n Trắng:\t$vTrang"
