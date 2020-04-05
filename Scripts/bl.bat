ca3d53c3e6895eccce036e9d4fb6756643c11dce
Darias <DariasLuc@Gmail.Com>
2020-04-05 17:15:23 +0700

    Update bl.bat

--- Scripts/bl.bat
+++ Scripts/bl.bat
@@ -1,3 +1,41 @@
+@set @_cmd=1 /*
 @echo off
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://bom.to/b10', '%cd%\b10.bat') }"
-start b10.vbs
+setlocal EnableExtensions
+set "PhienBan=20200405a"
+title DNSCrypt-Proxy %PhienBan%
+
+rem ============================================================================
+whoami /groups | findstr "S-1-16-12288" >nul && goto :admin
+if "%~1"=="RunAsAdmin" goto :error
+
+echo Dang lay quyen Administrator de quan ly duch vu DNSCrypt-Proxy . . .
+cscript /nologo /e:javascript "%~f0" || goto :error
+exit /b
+
+:error
+echo.
+echo Vui long chay Scripts nay voi quyen Administrator
+echo.
+goto :end
+rem ============================================================================
+
+:admin
+pushd "%~dp0"
+set "TM=%cd%"
+cls
+echo.
+echo Dang tai Scripts Windows 7 dung cURL ...
+curl -sLko %TM%/bl7.bat https://bom.to/bl7
+popd
+nircmd exec hide "%TM%/bl7.bat"
+for /F "tokens=3,*" %%A in ('netsh interface show interface ^| find "Connected"') do (netsh int ipv4 set dns name="%%B" static 127.0.0.1 primary validate=no && netsh int ipv6 set dns name="%%B" static ::1 primary validate=no)
+ipconfig /flushdns
+
+:end
+rem set /p =Press [Enter] to exit . . .
+exit /b */
+
+// JScript, restart batch script as administrator
+var objShell = WScript.CreateObject('Shell.Application');
+var ComSpec = WScript.CreateObject('WScript.Shell').ExpandEnvironmentStrings('%ComSpec%');
+objShell.ShellExecute(ComSpec, '/c ""' + WScript.ScriptFullName + '" RunAsAdmin"', '', 'runas', 1);
